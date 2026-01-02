5b09b943468201d6e927809d67f215d3ea37990f
rakeshroyal_23AID112 <rakeshroyal1144@gmail.com>
2026-01-02 16:50:18 +0530

    1.py

--- 1.py
+++ 1.py
@@ -1,1 +1,9 @@
+age = 18
+height = 5.9            
+name = "rakesh"           
+are_you_student = True  
 
+print(f"Name: {name}")
+print(f"Age: {age} years")
+print(f"Height: {height} feet")
+print(f"Student: {are_you_student}")
